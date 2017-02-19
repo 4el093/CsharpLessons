69738905201a49011fac55d96d9ebe40c1b06bfe
4el093 <iamdauren@mail.ru>
2017-02-20 00:37:48 +0600

    Add files via upload

--- Building.cs
+++ Building.cs
@@ -1,4 +1,5 @@
-﻿// Программа, в которой используется класс Building.
+﻿// В этой программе создаются два объекта типа Building.
+
 using System;
 
 class Building
@@ -8,25 +9,40 @@ class Building
     public int Occupants; // количество жильцов
 }
 
-// В этом классе объявляется объект типа Building.
+// В этом классе объявляются два объекта типа Building.
 class BuildingDemo
 {
     static void Main()
     {
-        Building house = new Building(); // создать объект типа Building
-        int areaPP; // площадь на одного человка
+        Building house = new Building();
+        Building office = new Building();
+
+        int areaPP; // площадь на одного человека
 
         // Присвоить значения полям в объекте house.
         house.Occupants = 4;
         house.Area = 2500;
         house.Floors = 2;
 
-        // Вычислить площадь на одного человка.
+        // Присвоить значения полям в объекте office.
+        office.Occupants = 25;
+        office.Area = 4200;
+        office.Floors = 3;
+
+        // Вычислить площадь на одного человека в жилом доме.
         areaPP = house.Area / house.Occupants;
-        Console.WriteLine("Дом имеет:\n " +
+
+        Console.WriteLine("Дом имеет\n " +
             house.Floors + " этажа\n " +
             house.Occupants + " жильца\n " +
             house.Area + " кв.футов общей площади, из них\n " +
+            areaPP + " приходится на одного человека");
+        // Вычислить площадь на одного человека в учереждении.
+        areaPP = office.Area / office.Occupants;
+        Console.WriteLine("Учереждение имеет\n " +
+            office.Floors + " этажа\n " +
+            office.Occupants + " работников\n " +
+            office.Area + " кв.футов общей площади, из них\n " +
             areaPP + " приходится на одного человека");
         Console.ReadKey();
     }
